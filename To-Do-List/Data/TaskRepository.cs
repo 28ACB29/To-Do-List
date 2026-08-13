0ff51307db0a959572c49871e05792303a8b2c6b
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-13 13:16:51 -0500

    Added Private Variables and Constructor

--- To-Do-List/Data/TaskRepository.cs
+++ To-Do-List/Data/TaskRepository.cs
@@ -5,10 +5,27 @@ namespace To_Do_List.Data
 {
 	public class TaskRepository : ITaskRepository
 	{
+
+		private readonly string filePath;
+
+		private readonly object @lock = new();
+
+		public TaskRepository(IHostEnvironment env)
+		{
+			string dir = Path.Combine(env.ContentRootPath, "wwwroot", "app-data");
+			Directory.CreateDirectory(dir);
+			this.filePath = Path.Combine(dir, "tasks.json");
+		}
+
+
 		public Models.Task Add(Models.Task task) => throw new NotImplementedException();
+
 		public bool Delete(int id) => throw new NotImplementedException();
+
 		public IEnumerable<Models.Task> GetAll() => throw new NotImplementedException();
+
 		public Models.Task? GetById(int id) => throw new NotImplementedException();
+
 		public bool Update(Models.Task task) => throw new NotImplementedException();
 	}
 }
