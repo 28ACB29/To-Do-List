48b30ed6d05659cfa3488a4a52622f0d0bf2bf9b
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-13 11:17:04 -0500

    Checking in the Frontend App module, which I forgot to do earlier.

--- To-Do-List-UI/src/app/app.module.ts
+++ To-Do-List-UI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskEditComponent } from './components/task-edit/task-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TaskListComponent,
+    TaskEditComponent
   ],
   imports: [
     BrowserModule,
