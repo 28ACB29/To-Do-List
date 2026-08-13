4074bf47c9c41e3ef5e83d8fa8992a2d7f6bca37
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-13 15:00:57 -0500

    Added Data Binding Imports to the Task Component.

--- To-Do-List-UI/src/app/components/task/task.component.ts
+++ To-Do-List-UI/src/app/components/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task, TaskStatus } from '../../models/task.model';
 
 @Component({
