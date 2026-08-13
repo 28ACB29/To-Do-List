d58cdb493d7959b19bf8985047b3bde18bf954f3
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-13 17:57:44 -0500

    Added missing Import.

--- To-Do-List-UI/src/app/services/task.service.ts
+++ To-Do-List-UI/src/app/services/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Task, TaskStatus } from '../models/task.model';
 
 const API_BASE = '/api/task'; // relative pa
