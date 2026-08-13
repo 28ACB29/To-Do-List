5eac6f6eae6227a23d422f7c28facd01b50ae9c3
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-13 13:12:39 -0500

    Start Building Repository.

--- To-Do-List/Data/ITaskRepository.cs
+++ To-Do-List/Data/ITaskRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace To_Do_List.Data
+﻿using To_Do_List.Models;
+
+namespace To_Do_List.Data
 {
-	public interface ITaskRepository
-	{
-	}
+    public interface ITaskRepository
+    {
+        IEnumerable<Models.Task> GetAll();
+        Models.Task? GetById(int id);
+        Models.Task Add(Models.Task task);
+        bool Update(Models.Task task);
+        bool Delete(int id);
+    }
 }
--- To-Do-List/Data/TaskRepository.cs
+++ To-Do-List/Data/TaskRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace To_Do_List.Data
+﻿using To_Do_List.Data;
+using To_Do_List.Models;
+
+namespace To_Do_List.Data
 {
-	public class TaskRepository
+	public class TaskRepository : ITaskRepository
 	{
+		public Models.Task Add(Models.Task task) => throw new NotImplementedException();
+		public bool Delete(int id) => throw new NotImplementedException();
+		public IEnumerable<Models.Task> GetAll() => throw new NotImplementedException();
+		public Models.Task? GetById(int id) => throw new NotImplementedException();
+		public bool Update(Models.Task task) => throw new NotImplementedException();
 	}
 }
