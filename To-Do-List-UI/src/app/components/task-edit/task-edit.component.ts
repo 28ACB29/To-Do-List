ae8e9a8c714f1209913bed72990206c9f4ba6c23
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-13 11:15:04 -0500

    Checking in Frontend Edit and List Components. These may be unnecessary, and I may delete them if I find no use for them.

---
+++ To-Do-List-UI/src/app/components/task-edit/task-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-edit',
+  standalone: false,
+  templateUrl: './task-edit.component.html',
+  styleUrl: './task-edit.component.css'
+})
+export class TaskEditComponent {
+
+}
