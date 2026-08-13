6d137286a8f8777928428ada416f15e1a93c8f83
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-13 11:08:03 -0500

    Checking in Backend Model Classes.

---
+++ To-Do-List/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public TaskStatus Status { get; set; }
+    }
+}
---
+++ To-Do-List/Models/TaskStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Models
+{
+	public enum TaskStatus
+	{
+		New,
+		InProgress,
+		Rejected,
+		Verified,
+		Completed
+	}
+}
