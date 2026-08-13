378bc398aeff6faa556074c62eaae3f182ac24a2
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-13 17:52:05 -0500

    Added Minimal Task Service Implementation.

--- To-Do-List-UI/src/app/services/task.service.ts
+++ To-Do-List-UI/src/app/services/task.service.ts
@@ -3,11 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Task, TaskStatus } from '../models/task.model';
 
+const API_BASE = '/api/task'; // relative pa
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class TaskService {
 
-  constructor() { }
+  private tasksSubject = new BehaviorSubject<Task[]>([]);
+
+  public tasks$ = this.tasksSubject.asObservable();
+
+  constructor(private http: HttpClient) {
+    this.load();
+  }
+
+  private load(): void {
+    this.http.get<Task[]>(API_BASE)
+      .subscribe({
+        next: tasks => this.tasksSubject.next(tasks),
+        error: () => this.tasksSubject.next([])
+      });
+  }
 }
